350c75eabc3e2cd979a484b2ca5b8bf4cb8d0d37
Marcin Blaszczak <marcin@kudlaty.eu>
2023-01-10 20:11:28 +0100

    Nowa bardziej C# wersja

--- kudlaty1.Module/BusinessObjects/Firma.cs
+++ kudlaty1.Module/BusinessObjects/Firma.cs
@@ -1,4 +1,5 @@
-﻿using DevExpress.ExpressApp.DC;
+﻿using DevExpress.CodeParser;
+using DevExpress.ExpressApp.DC;
 using DevExpress.ExpressApp.Model;
 using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl;
@@ -8,14 +9,19 @@ using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization.Json;
 using System.Security.Policy;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 
 
 namespace kudlaty1.Module.BusinessObjects
 {
+    // Czynny, Zwolniony, Niezarejestrowany
+    public enum StatusVat { Nieznane = -1, Czynny = 1, Zwolniony = 2, Niezarejestrowany = 3 }
+
     [DefaultClassOptions]
     [XafDefaultProperty(nameof(Nazwa))]
     [NavigationItem("Operacje")]
@@ -33,6 +39,7 @@ namespace kudlaty1.Module.BusinessObjects
         string regon;
         string nip;
         string nazwa;
+        
 
         [Size(200)]
         public string Nazwa
@@ -60,10 +67,6 @@ namespace kudlaty1.Module.BusinessObjects
             get => regon;
             set => SetPropertyValue(nameof(Regon), ref regon, value);
         }
-
-
-
-
 
         [Size(SizeAttribute.DefaultStringMappingFieldSize)]
         [XafDisplayName("KRS")]
@@ -102,82 +105,115 @@ namespace kudlaty1.Module.BusinessObjects
         }
 
         [Action(ToolTip = "Pobiera informacje o firmie z danych Ministerstwa Finansow", Caption = "Pobierz Dane z MF")]
-        public async void pobierzDane()
+        public  async void wypelnijDane()
         {
-            var klient = new HttpClient();
-            try {
-                if (Nip == null) Nip = "";
-                if (Regon == null) Regon = "";
-                var adres = new Uri("https://wl-api.mf.gov.pl/api/search/nip/" + Nip.ToString() + "?date=2023-01-08");
-                
-                if(Nip=="" & Regon!="")
-                {
-                    adres = new Uri("https://wl-api.mf.gov.pl/api/search/regon/" + Regon.ToString() + "?date=2023-01-08");
-                }
-                if (Nip != "" || Regon != "")
-                {
-                    var dane_tmp = await klient.GetStringAsync(adres);
-                    var dane = JsonObject.Parse(dane_tmp);
-                    //this.nazwa=dane["Subject"]["Name"].ToString();
-                    if (dane["result"]["subject"]["name"] != null) Nazwa = dane["result"]["subject"]["name"].ToString();
-                    else Nazwa = "Brak nazwy";
-                    if (dane["result"]["subject"]["regon"] != null) Regon = dane["result"]["subject"]["regon"].ToString();
-                    else Regon = "";
-                    if (dane["result"]["subject"]["nip"] != null) Nip = dane["result"]["subject"]["nip"].ToString();
-                    else Nip = "";
-                    if (dane["result"]["subject"]["krs"] != null) Krs = dane["result"]["subject"]["krs"].ToString();
-                    else Krs = "";
-                    if (dane["result"]["subject"]["residenceAddress"] != null) AdresSiedziby = dane["result"]["subject"]["residenceAddress"].ToString();
-                    else AdresSiedziby = "";
-                    if (dane["result"]["subject"]["workingAddress"] != null) AdresWykonywaniaDzialalnosci = dane["result"]["subject"]["workingAddress"].ToString();
-                    else AdresWykonywaniaDzialalnosci = "";
-                    if (dane["result"]["subject"]["registrationLegalDate"] != null) DataRozpoczeciaDzialanosci = DateTime.Parse(dane["result"]["subject"]["registrationLegalDate"].ToString());
-                    // Status VAT traktuje inaczej ;)
+            OdpowiedzMF? odpowiedzMF;
+            var ministerstwo = new Ministerstwo();
+            string adres;
 
-                    // Czynny, Zwolniony, Niezarejestrowany
-                    if (dane["result"]["subject"]["statusVat"].ToString() == "Czynny") StatusVat = StatusVat.Czynny;
-                    if (dane["result"]["subject"]["statusVat"].ToString() == "Zwolniony") StatusVat = StatusVat.Zwolniony;
-                    if (dane["result"]["subject"]["statusVat"].ToString() == "Niezarejestrowany") StatusVat = StatusVat.Niezarejestrowany;
-                }
-                else Nazwa = "Wypelnij NIP lub REGON";
+            if (ministerstwo.sprawdzNIP(Nip)) adres = "https://wl-api.mf.gov.pl/api/search/nip/" + Nip;
+            else if (ministerstwo.sprawdzREGON(Regon)) adres = "https://wl-api.mf.gov.pl/api/search/regon/" + Regon;
+            else 
+            {
+
+                Nazwa = "Podaj poprawny NIP lub REGON";
+                return;
+            }
+            adres = adres + "?date=" + DateTime.Now.ToString("yyyy-MM-dd");
+
+            try
+            {
+                var klient = new HttpClient();
+                var dane_tmp = await klient.GetStringAsync(new Uri(adres));
+                odpowiedzMF = JsonSerializer.Deserialize<OdpowiedzMF>(dane_tmp);
+                Nazwa = odpowiedzMF.result.subject.name;
+                Nip = odpowiedzMF.result.subject.nip;
+                Krs = odpowiedzMF.result.subject.krs;
+                Regon = odpowiedzMF.result.subject.regon;
+                AdresSiedziby = odpowiedzMF.result.subject.residenceAddress;
+                AdresWykonywaniaDzialalnosci = odpowiedzMF.result.subject.workingAdress;
+
+                if(odpowiedzMF.result.subject.statusVat=="Czynny") StatusVat = StatusVat.Czynny;
+                else if(odpowiedzMF.result.subject.statusVat == "Zwolniony") StatusVat = StatusVat.Zwolniony;
+                else if(odpowiedzMF.result.subject.statusVat == "Niezarejestrowany") StatusVat = StatusVat.Niezarejestrowany;
 
             }
             catch (Exception ex)
             {
-                Nazwa = "Bledny NIP";
-                Krs = "";
-                Regon = "";
-                Nip = "";
-                AdresSiedziby = "";
-                AdresWykonywaniaDzialalnosci = "";
-                DataRozpoczeciaDzialanosci = DateTime.Parse("1970-01-01");
-                StatusVat = StatusVat.Nieznane;
+                Nazwa = "Wystapil blad pobierania danych " + ex.Message;
+
             }
+
         }
-        
 
 
-        /*
-                [DefaultClassOptions]
-                [ModelDefault("Caption", "Task")]
-                public class DemoTask : Task
-                {
-                    //...
 
-                    [Action(ToolTip = "Postpone the task to the next day", Caption = "Postpone")]
-                    //Shift the task's due date forward by one day
-                    public void Postpone()
+
+
+
+            /* Jak to robia ludzie kudlaci
+
+            [Action(ToolTip = "Pobiera informacje o firmie z danych Ministerstwa Finansow", Caption = "Pobierz Dane z MF")]
+            public async void pobierzDane()
+            {
+                var klient = new HttpClient();
+                try {
+                    if (Nip == null) Nip = "";
+                    if (Regon == null) Regon = "";
+                    var adres = new Uri("https://wl-api.mf.gov.pl/api/search/nip/" + Nip.ToString() + "?date=2023-01-08");
+
+                    if(Nip=="" & Regon!="")
                     {
-
+                        adres = new Uri("https://wl-api.mf.gov.pl/api/search/regon/" + Regon.ToString() + "?date=2023-01-08");
                     }
+                    if (Nip != "" || Regon != "")
+                    {
+                        var dane_tmp = await klient.GetStringAsync(adres);
+                        var dane = JsonObject.Parse(dane_tmp);
+                        //this.nazwa=dane["Subject"]["Name"].ToString();
+                        if (dane["result"]["subject"]["name"] != null) Nazwa = dane["result"]["subject"]["name"].ToString();
+                        else Nazwa = "Brak nazwy";
+                        if (dane["result"]["subject"]["regon"] != null) Regon = dane["result"]["subject"]["regon"].ToString();
+                        else Regon = "";
+                        if (dane["result"]["subject"]["nip"] != null) Nip = dane["result"]["subject"]["nip"].ToString();
+                        else Nip = "";
+                        if (dane["result"]["subject"]["krs"] != null) Krs = dane["result"]["subject"]["krs"].ToString();
+                        else Krs = "";
+                        if (dane["result"]["subject"]["residenceAddress"] != null) AdresSiedziby = dane["result"]["subject"]["residenceAddress"].ToString();
+                        else AdresSiedziby = "";
+                        if (dane["result"]["subject"]["workingAddress"] != null) AdresWykonywaniaDzialalnosci = dane["result"]["subject"]["workingAddress"].ToString();
+                        else AdresWykonywaniaDzialalnosci = "";
+                        if (dane["result"]["subject"]["registrationLegalDate"] != null) DataRozpoczeciaDzialanosci = DateTime.Parse(dane["result"]["subject"]["registrationLegalDate"].ToString());
+                        // Status VAT traktuje inaczej ;)
+
+                        // Czynny, Zwolniony, Niezarejestrowany
+                        if (dane["result"]["subject"]["statusVat"].ToString() == "Czynny") StatusVat = StatusVat.Czynny;
+                        if (dane["result"]["subject"]["statusVat"].ToString() == "Zwolniony") StatusVat = StatusVat.Zwolniony;
+                        if (dane["result"]["subject"]["statusVat"].ToString() == "Niezarejestrowany") StatusVat = StatusVat.Niezarejestrowany;
+                    }
+                    else Nazwa = "Wypelnij NIP lub REGON";
+
                 }
-        */
+                catch (Exception ex)
+                {
+                    Nazwa = "Bledny NIP";
+                    Krs = "";
+                    Regon = "";
+                    Nip = "";
+                    AdresSiedziby = "";
+                    AdresWykonywaniaDzialalnosci = "";
+                    DataRozpoczeciaDzialanosci = DateTime.Parse("1970-01-01");
+                    StatusVat = StatusVat.Nieznane;
+                }
+            }
+            // Koniec przykladu wg ludzi kudlatych
+            */
 
-    }
+
+        }
 
 
 
-    // Czynny, Zwolniony, Niezarejestrowany
-    public enum StatusVat { Nieznane=-1, Czynny=1, Zwolniony=2, Niezarejestrowany = 3 }
+
 
 }
---
+++ kudlaty1.Module/BusinessObjects/Ministerstwo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace kudlaty1.Module.BusinessObjects
+{
+    public class Subject
+    {
+        private string _workingAdress;
+        private string _residenceAddress;
+        private string _krs;
+        private string _regon;
+        private DateTime _registrationLegalDate;
+        private string _nip;
+        private string _name;
+        
+        public string workingAdress { get=>_workingAdress; set { _workingAdress = value ?? ""; } }
+        public string residenceAddress { get => _residenceAddress; set { _residenceAddress = value ?? String.Empty; } }
+        public string krs { get => _krs; set { _krs = value ?? "Brak wpisu w KRS"; } }
+        public string regon { get => _regon; set { _regon = value ?? String.Empty; } }
+        public DateTime registrationLegalDate { get; set; }
+        public string nip { get; set; }
+        public string name { get; set; }
+
+        public string statusVat { get; set; }
+    }
+
+    public class Result
+    {
+        public Subject subject { get; set; }
+        public string requestDateTime { get; set; }
+        public string requestId { get; set; }
+    }
+
+    public class OdpowiedzMF
+    {
+        private string _code;
+        public string code { get => _code; set { _code = value ?? "ok"; } }
+        public string? message { get; set; }
+        public Result result { get; set; }
+
+    }
+
+    public class Ministerstwo
+    { 
+
+        //zostawiam stary konstruktor, moze sie przyda ;)
+        public Ministerstwo() { }
+       
+        public bool sprawdzREGON(string REGON)
+        {
+            //UWAGA - ignorowane sa REGONY 14-to cyfrowe!!!!!
+            int suma = 0;
+            int[] wagi = { 8, 9, 2, 3, 4, 5, 6, 7 };
+            if (REGON.Length != 9) return false;
+            for (int i = 0; i < 9; i++)
+            {
+                if (!Char.IsDigit(REGON[i])) return false;
+                if (i < 8)
+                {
+                    suma = suma + (int)Char.GetNumericValue(REGON[i]) * wagi[i];
+                }
+            }
+            if (suma % 11 == (int)Char.GetNumericValue(REGON[8])) return true;
+            return false;
+        }
+
+        public bool sprawdzNIP(string NIP)
+        {
+            int suma = 0;
+            int[] wagi = { 6, 5, 7, 2, 3, 4, 5, 6, 7 };
+            if (NIP.Length != 10) return false;
+            for(int i=0; i < 10; i++)
+            {
+                if (!Char.IsDigit(NIP[i])) return false;
+                if(i<9)
+                {
+                    suma = suma + (int)Char.GetNumericValue(NIP[i]) * wagi[i];
+                }
+            }
+            if (suma%11 == (int)Char.GetNumericValue(NIP[9])) return true;
+
+            return false;
+        }
+
+    }
+}
